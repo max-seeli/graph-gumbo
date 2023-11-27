d471c4e72c1a8d9c83bdfb66d253fda905b7d2cb
Max Seeliger <maximilian.seeliger@gmail.com>
2023-11-27 23:26:47 +0100

    Added visualizations

--- visual.py
+++ visual.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import os
 
 def plot_graph(graph, title=None, figsize=(5, 5)):
     """
@@ -40,3 +41,19 @@ def plot_horizontally(graphs, titles=None, figsize=None):
             axes[i].set_title(titles[i])
     plt.show()
 
+def save_pdf(fig, name):
+    """
+    Saves a figure in the pdf format at 'img/plots'.
+
+    Parameters
+    ----------
+    fig : matplotlib.figure.Figure
+        The figure to save
+    name : str
+        The name of the file to save the figure to
+    """
+    if not os.path.exists('img/plots'):
+        os.makedirs('img/plots')
+    file_name = os.path.join('img/plots', name + '.pdf')
+
+    fig.savefig(file_name, format='pdf', dpi=300, bbox_inches='tight',)
